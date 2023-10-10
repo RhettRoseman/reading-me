b82ce70e126b1f391e878b1e8a03863699a15183
RhettRoseman <rroseman@rcplanners.com>
2023-10-09 20:08:46 -0400

    index.js finished project ready to be recorded

---
+++ README.md
@@ -0,0 +1,32 @@
+"# Build CLI App Inquirer"
+Rhett;
+
+## Description
+
+Work with a partner to implement the following user story:
+* My name is Rhett   As a developer, I want to create a command-line application that takes in input from the user and does something with it.
+## Description 
+IF you go to your terminal/bash window 
+THEN you go type in <node index.js> 
+WHEN you type in <node index.js>
+THEN you will be met with a series of prompts 
+WHEN you fill out the prompts 
+THEN you will see "Success!" and will have generated a README file
+
+## Author
+RhettRoseman
+## Technologies
+node.js
+JSON 
+node.js
+
+## Links 
+github.com/RhettRoseman
+
+## Preffered Method of contact 
+telekinesis
+
+
+
+
+
--- index.js
+++ index.js
@@ -1,6 +1,34 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const html = ({name, stack, username, contact, repo,}) => 
+`"# Build CLI App Inquirer"
+${name};
 
+## Description
+
+Work with a partner to implement the following user story:
+* My name is ${name}   As a developer, I want to create a command-line application that takes in input from the user and does something with it.
+## Description 
+IF you go to your terminal/bash window 
+THEN you go type in <node index.js> 
+WHEN you type in <node index.js>
+THEN you will be met with a series of prompts 
+WHEN you fill out the prompts 
+THEN you will see "Success!" and will have generated a README file
+
+## Author
+${username}
+## Technologies
+node.js
+JSON 
+${stack}
+
+## Links 
+${repo}
+
+## Preffered Method of contact 
+${contact} 
+`
 inquirer
   .prompt([
     {
@@ -12,7 +40,7 @@ inquirer
       type: 'checkbox',
       message: 'What languages do you know?',
       name: 'stack',
-      choices: ['HTML', 'CSS', 'JavaScript', 'MySQL'],
+      choices: ['HTML', 'CSS', 'JavaScript', 'node.js', 'MySQL'],
     },
     {
       type: 'list',
@@ -20,13 +48,27 @@ inquirer
       name: 'contact',
       choices: ['email', 'phone', 'telekinesis'],
     },
+ 
+{
+  type: 'input',
+   message: "What is your GitHub username? (No @ needed)",
+  name: 'username',
+},
+{
+  type: 'input',
+  message: "What is the name of your GitHub repo?",
+  name: 'repo',
+},
+
   ])
-  .then((data) => {
-    console.log(data);
+  .then((answers) => {
+   const htmlPageContent = html(answers);
 
-    const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
+    const filename = "README.md";
 
-    fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
-      err ? console.log(err) : console.log('Success!')
-    );
-  });
+    fs.writeFile(filename,htmlPageContent, (err) =>
+      err ? console.log(err) : console.log('Success!'),
+
+
+    )});
+  // "${answers.username.toLowerCase().split(' ').join('')
